ed74b8ae3a271abbdecab1f468ec57a6809a7856
UrosZikic <uroszikic1512988@gmail.com>
2023-10-27 17:23:56 +0200

    input update

--- src/Contact.js
+++ src/Contact.js
@@ -30,13 +30,14 @@ export default function ContactUs() {
     <section className="contact" id="contact">
       <h2>Let's Connect!</h2>
       <form ref={form} onSubmit={sendEmail}>
-        <label>Name</label>
-        <input type="text" name="user_name" />
-        <label>Email</label>
-        <input type="email" name="user_email" />
-        <label>Message</label>
-        <textarea name="message" />
-        <input type="submit" value="Send" />
+        <label htmlFor="user_name">Name</label>
+        <input type="text" name="user_name" id="user_name" required />
+        <label htmlFor="user_email">Email</label>
+        <input type="email" name="user_email" id="user_email" required />
+        <label htmlFor="message">Message</label>
+        <textarea name="message" id="message" required />
+        <label htmlFor="sub"></label>
+        <input type="submit" name="sub" value="Send" id="sub" />
       </form>
     </section>
   );
