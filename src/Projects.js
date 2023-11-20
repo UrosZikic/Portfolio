03f94bebd27985ffae7075621663eb4d6d270358
UrosZikic <uroszikic1512988@gmail.com>
2023-11-20 18:28:57 +0100

    projects update

--- src/Projects.js
+++ src/Projects.js
@@ -3,9 +3,9 @@ import React from "react";
 //import images
 import projectImage from "./images/project-1.png";
 import projectImage2 from "./images/project-2.jpg";
-import projectImage3 from "./images/project-3.png";
+import projectImage3 from "./images/project-3.jpg";
 import projectImage4 from "./images/project-4.jpg";
-import projectImage5 from "./images/project-4.png";
+import projectImage5 from "./images/project-5.png";
 // import styles
 import "./styles/Projects.css";
 
